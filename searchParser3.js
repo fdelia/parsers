105a5893e1ce6f6875f9583ef89fa0eebfbf321c
fdelia <fabio_d@gmx.net>
2018-01-27 12:10:26 +0100

    save

--- searchParser3.js
+++ searchParser3.js
@@ -1,23 +1,24 @@
+/* Not functional! */
 /* eslint quotes: 0 */
 /* eslint semi: 0 */
 'use strict';
 
-// Order gives precedence of operators, higher = higher = first handled
-const opMap = {
-  "!": "NOT",
-  "+": "AND",
-  "|": "OR"
-}
-
 // Types
 const OpPrefix = Symbol("OpPrefix")
-const OpPostfix = Symbol("OpPostfix")
+// const OpPostfix = Symbol("OpPostfix") // we have no OpPostfix with this language specifications
 const OpInfix = Symbol("OpInfix")
 const Int = Symbol("Int")
-const String = Symbol("String")
+const Str = Symbol("String")
+const Token = Symbol("Token")
+
+// Order gives precedence of operators, higher = higher = first handled
+const opMap = new Map();
+opMap.set("!", [OpPrefix, "NOT"])
+// opMap.set("+", [OpInfix, "AND"])
+// opMap.set("|", [OpInfix, "OR"])
 
 class Node {
-  constructor(type, value, children = []){
+  constructor (type, value, children = []) {
     this.type = type // see Types above
     this.value = value // int or string
     this.children = children // []
@@ -26,8 +27,11 @@ class Node {
 
 class Tokens {
   constructor (tokens) {
-    this.tokens = tokens
+    this.tokens = tokens.map(x => new Node(Token, x))
     this.c = 0
+  }
+  finished () {
+    return this.tokens.every(node => typeof (node) === typeof (Node))
   }
   get () {
     return this.tokens[this.c]
@@ -40,16 +44,33 @@ class Tokens {
   }
 }
 
-const QueryFromExpression = searchExpression => {
-  const tokens = Tokens(searchExpression.split(" ").map(x => x.trim()));
+const QueryFromExpression = (searchExpression, fieldName) => {
+  var tokens = new Tokens(searchExpression.split(" ").map(x => x.trim()));
   console.log(tokens);
 
-  var parent = Node(OpPrefix, null)
-  while (tokens.peek()) {
+  opMap.forEach((opProp, op) => {
+    while (tokens.peek()) {
+      if (opProp[0] === OpPrefix) {
+        switch (tokens.peek().type) {
+          case OpPrefix:
+          case OpInfix:
+            break;
+          case Int:
+          case Str:
+          case Token:
+            break;
+        }
+        // if is type string -> Node
 
-  }
+      }
+      if (opProp[0] === OpInfix) {
+
+      }
+      tokens.next()
+    }
+  })
 }
 
-console.log(QueryFromExpression("x | y z", "address")) // x OR (y AND z)
-console.log(QueryFromExpression("v ! x | y | ! z", "address"))
-console.log(QueryFromExpression("3 | > 10 < 20", "address"))
+console.log(QueryFromExpression("x | y z", "name")) // x OR (y AND z)
+console.log(QueryFromExpression("v ! x | y | ! z", "name"))
+console.log(QueryFromExpression("3 | > 10 < 20", "name"))
