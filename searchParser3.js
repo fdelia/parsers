ee46196c015e9846a13c8f594da876ea15a236a7
fdelia <fabio_d@gmx.net>
2018-01-27 13:24:20 +0100

    save

--- searchParser3.js
+++ searchParser3.js
@@ -9,12 +9,12 @@ const OpPrefix = Symbol("OpPrefix")
 const OpInfix = Symbol("OpInfix")
 const Int = Symbol("Int")
 const Str = Symbol("String")
-const Token = Symbol("Token")
+// const Token = Symbol("Token")
 
 // Order gives precedence of operators, higher = higher = first handled
 const opMap = new Map();
 opMap.set("!", [OpPrefix, "NOT"])
-// opMap.set("+", [OpInfix, "AND"])
+opMap.set("+", [OpInfix, "AND"])
 opMap.set("|", [OpInfix, "OR"])
 
 class Node {
@@ -27,10 +27,10 @@ class Node {
 
 // Already matches operators, however with no logic
 const Tokens = tokens => {
-  this.tokens = tokens.map(token => {
+  this.tokens = tokens /* .map(token => {
     var type = opMap.has(token) ? opMap.get(token)[0] : (/^\d+$/.test(token) ? Int : Str);
     return new Node(type, token);
-  });
+  }); */
   this.c = 0
 
   this.finished = () => this.tokens.every(node => typeof (node) === typeof (Node))
@@ -38,38 +38,63 @@ const Tokens = tokens => {
   this.set = (newToken) => { this.tokens[this.c] = newToken; return null; }
   this.peek = () => this.tokens.length > this.c ? this.tokens[this.c + 1] : null;
   this.setPeek = (newToken) => { this.tokens[this.c + 1] = newToken; return null; }
-  this.next = () => this.tokens[++this.c]
+  this.move = () => this.tokens.length > this.c + 1 ? this.tokens[++this.c] : null
+  this.before = () => this.tokens[this.c - 1]
+
+  this.get = d => this.tokens.length > this.c + d ? this.tokens[this.c + d] : null
+  this.remove = d => this.tokens.splice(this.c + d, 1) // remove that one
 
   return this;
 }
 
 const QueryFromExpression = (searchExpression, fieldName) => {
-  var tokens = Tokens(searchExpression.split(" ").map(x => x.trim()));
-  console.log(tokens);
+  // Lexer
+  var tokens = searchExpression.split(" ").map(x => x.trim()).map(token => {
+    var type = opMap.has(token) ? opMap.get(token)[0] : (/^\d+$/.test(token) ? Int : Str);
+    return new Node(type, token);
+  });
+  // Token handler
+  tokens = Tokens(tokens);
 
   // Parse logic
+  var newNodes = []
   opMap.forEach((opProp, op) => {
-    while (tokens.peek()) {
-      if (opProp[0] === OpPrefix) {
-        switch (tokens.peek().type) {
-          case OpPrefix:
-          case OpInfix:
-            break;
-          case Int:
-          case Str:
-            break;
-        }
-        // if is type string -> Node
+    // console.log('op ' + op)
 
+    while (tokens.move()) { // start at position 1
+      if (tokens.get(0).value !== op) {
+        newNodes.push(tokens.get(-1))
+        continue;
       }
-      if (opProp[0] === OpInfix) {
+      // console.log(' found at ' + tokens.c)
 
+      switch (tokens.get(0).type) {
+        case OpPrefix:
+          newNodes.push(new Node(tokens.get(0).type, tokens.get(0).value, [tokens.get(1)]))
+          tokens.remove(0)
+          tokens.remove(1)
+          break;
+        case OpInfix:
+          newNodes.push(new Node(tokens.get(0).type, tokens.get(0).value, [tokens.before(), tokens.get(1)]))
+          tokens.remove(-1)
+          tokens.remove(0)
+          tokens.remove(1)
+          break;
       }
-      tokens.next()
     }
+    // console.log(newNodes);
+    tokens = Tokens(newNodes)
   })
+  // console.log(tokens.tokens)
+  return tokens.tokens;
 }
 
-console.log(QueryFromExpression("x | y z", "name")) // x OR (y AND z)
-console.log(QueryFromExpression("v ! x | y | ! z", "name"))
-console.log(QueryFromExpression("3 | > 10 < 20", "name"))
+const PrintAST = (ast, level = 0) => {
+  console.log(ast)
+  // console.log(ast.type + " -> " + ast.value)
+  // if (ast.children.length > 0) ast.children.forEach(child => PrintAST(child, level + 1))
+}
+
+console.log(QueryFromExpression("x | y ! z", "name")) // x OR (y AND z)
+// console.log(QueryFromExpression("v ! x | y | ! z", "name"))
+// console.log(QueryFromExpression("3 | > 10 < 20", "name"))
