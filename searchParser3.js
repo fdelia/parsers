639e510272abe23bb0d06f1f60dccd047e3d038f
fdelia <fabio_d@gmx.net>
2018-01-28 09:41:22 +0100

    save

--- searchParser3.js
+++ searchParser3.js
@@ -9,12 +9,11 @@ const OpPrefix = Symbol("OpPrefix")
 const OpInfix = Symbol("OpInfix")
 const Int = Symbol("Int")
 const Str = Symbol("String")
-// const Token = Symbol("Token")
 
-// Order gives precedence of operators, higher = higher = first handled
+// Order gives precedence of operators, higher = first handled
 const opMap = new Map();
 opMap.set("!", [OpPrefix, "NOT"])
-// opMap.set("+", [OpInfix, "AND"])
+opMap.set("+", [OpInfix, "AND"])
 opMap.set("|", [OpInfix, "OR"])
 
 class Node {
@@ -25,18 +24,14 @@ class Node {
   }
 }
 
-// Already matches operators, however with no logic
+// Tokens handler
 const Tokens = tokens => {
   this.tokens = tokens
   this.c = 0
 
   this.finished = () => this.tokens.every(node => typeof (node) === typeof (Node))
-  // this.get = () => this.tokens[this.c]
   this.set = (newToken) => { this.tokens[this.c] = newToken; return null; }
-  // this.peek = () => this.tokens.length > this.c ? this.tokens[this.c + 1] : null;
-  // this.setPeek = (newToken) => { this.tokens[this.c + 1] = newToken; return null; }
   this.move = () => this.tokens.length > this.c + 1 ? this.tokens[++this.c] : null
-  // this.before = () => this.tokens[this.c - 1]
 
   this.get = d => this.tokens.length > this.c + d ? this.tokens[this.c + d] : null
   this.remove = d => {
@@ -49,85 +44,57 @@ const Tokens = tokens => {
 }
 
 const QueryFromExpression = (searchExpression, fieldName) => {
+  console.log(searchExpression);
+
   // Lexer
   var tokens = searchExpression.split(" ").map(x => x.trim()).map(token => {
     var type = opMap.has(token) ? opMap.get(token)[0] : (/^\d+$/.test(token) ? Int : Str);
     return new Node(type, token);
   });
+
   // Token handler
   tokens = Tokens(tokens);
 
   // Parse logic
-
   opMap.forEach((opProp, op) => {
     tokens.c = 0
-    console.log('op ' + op)
-    // var newNodes = []
+    // console.log('op ' + op)
 
     while (tokens.move() !== null) { // start at position 1
       if (!tokens.get(0)) continue;
-      if (tokens.get(0).value !== op) {
-        // newNodes.push(tokens.get(-1))
-        // console.log(tokens.get(0).value)
+      if (tokens.get(0).value !== op) continue;
+      // console.log(' found at ' + tokens.c)
 
-        // if (!tokens.get(1) /* || tokens.get(1).type !== OpInfix */) newNodes.push(tokens.get(0))
-        continue;
-      }
-      console.log(' found at ' + tokens.c)
-
-      switch (tokens.get(0).type) {
+      var curr = tokens.get(0)
+      switch (curr.type) {
         case OpPrefix:
-          // newNodes.push(tokens.get(-1))
-          var curr = tokens.get(0)
           tokens.set(new Node(curr.type, curr.value, [tokens.get(1)]))
-          // tokens.remove(0)
           tokens.remove(1)
           break;
         case OpInfix:
-          var curr = tokens.get(0)
           tokens.set(new Node(curr.type, curr.value, [tokens.get(-1), tokens.get(1)]))
           tokens.remove(-1)
-          // tokens.remove(0)
           tokens.remove(1)
           break;
       }
     }
-    console.log(tokens.tokens);
-    // tokens = Tokens(newNodes)
+    // console.log(tokens.tokens);
   })
 
-  /* const parseNodes = (op, before, current, next) => {
-    if (!next) return [before, current]
-    if (tokens.get(0).value !== op) {
-      return [before, current];
-    }
-  }
-
-  opMap.forEach((opProp, op) => {
-    var newNodes = []
-    while (tokens.move() !== null) { // start at position 1
-      newNodes = newNodes.concat(parseNodes(op, tokens.get(-1), tokens.get(0), tokens.get(1)))
-    }
-    console.log(newNodes);
-    tokens = Tokens(newNodes)
-  }) */
-
-  // tokenNodes -> parsedNodes
-  // parseNodes(-1, 0, 1) -> [a, b, c]
-
-  return tokens.tokens.filter(x => x);
+  return tokens.tokens; // .filter(x => x);
 }
 
 const PrintAST = (ast, level = 0) => {
-  // console.log(ast)
-  if (ast.length > 0) ast = ast[1]
-  console.log(" - ".repeat(level) + String(ast.type) + " -> " + ast.value)
-  // console.log(ast.children)
+  if (ast.length > 0) ast = ast[0] // can be an array
+  console.log("   ".repeat(level) + String(ast.type) + " -> " + ast.value)
   if (ast.children.length > 0) ast.children.forEach(child => PrintAST(child, level + 1))
 }
 
-var ast = QueryFromExpression("x | y | z", "name");
+// var ast = QueryFromExpression("x | y + z", "name");
 // var ast = QueryFromExpression("x | ! y", "name");
-PrintAST(ast.length > 0 ? ast[0] : ast) // x OR (y AND z)
+// PrintAST(ast.length > 0 ? ast[0] : ast) // x OR (y AND z)
+
+var ast2 = QueryFromExpression("v | ! x | y | ! z", "name")
+PrintAST(ast2)
 // console.log(QueryFromExpression("v ! x | y | ! z", "name"))
 // console.log(QueryFromExpression("3 | > 10 < 20", "name"))
